f9013e85538c608111e6d0c14e9c18ec6a70fbc3
David Moler <tigermoler@gmail.com>
2020-10-24 09:59:45 -0700

    fixed errors

--- express.js
+++ express.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express()
+const path = require("path")
 
 let PORT = process.env.PORT || 40545;
 
 
 app.get("*", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
   });
   
   app.get("/notes", function(req, res) {
-    res.json(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
   });
   
 
